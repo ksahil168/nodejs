81d9917ac6517fa45eaed858dd3fb0f26ac68c85
Kumar Sahil <2270168@kiit.ac.in>
2024-02-11 11:02:43 +0530

    part 1

--- import exports/script.js
+++ import exports/script.js
@@ -1,3 +1,4 @@
+// This code snippet imports the module "script2" and assigns it to the variable "a". It then logs the value of "a" to the console.
 var a = require("./script2");
 
 console.log(a);
--- import exports/script2.js
+++ import exports/script2.js
@@ -1,6 +1,7 @@
 var a = 2;
 var b = 34;
 var c = 56;
+// This code snippet exports variables "a" and "b" as an object and variable "c" individually. The commented out line logs the value of variable "b" to the console.
 
 //for exporting one variable
 module.exports = c;
