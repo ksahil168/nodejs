4bf9beff06922cc7473d93867d11f2503336ebd7
Kumar Sahil <2270168@kiit.ac.in>
2024-02-15 00:39:22 +0530

    2

---
+++ express generator/script.js
@@ -0,0 +1,5 @@
+// express generator  ek folder bana ke deta hai , jiska matlb apko khudse folder nahi banana padta , express generator sare files ko is folder me daal ke deta hai.
+
+// steps to use express generator 
+//ek baar install krna padega globally
+//npm i express-generator -g
